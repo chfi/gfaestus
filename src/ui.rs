4d8d38a4787f678d971815490b1d38a53ed5044b
Christian Fischer <christian@chfi.se>
2021-02-09 21:36:56 -0600

    better scaling

--- src/ui.rs
+++ src/ui.rs
@@ -36,8 +36,8 @@ impl UIThread {
                 last_time = std::time::Instant::now();
                 since_last_update += delta;
 
-                // if since_last_update > 1.0 / 75.0 {
-                if since_last_update > 0.0001 {
+                if since_last_update > 1.0 / 75.0 {
+                    // if since_last_update > 0.0001 {
                     buf_ui_state.update_anim(since_last_update);
                     since_last_update = 0.0;
                 }
@@ -126,12 +126,33 @@ impl UIState {
     pub fn update_anim(&mut self, t: f32) {
         // println!("delta {}", t);
         // println!("fps {}", 1.0 / t);
-        let zoom_friction = 0.999999;
+        // let zoom_friction = 0.9999;
+        // let delta_mult = self.view.scale.log2();
+        // let scale_d = self.anim.scale_delta;
+        // let zoom_friction = 1.0 - (scale_d * t);
 
-        // let pan_friction = 1.0 - (1.0 + 0.5 * t).log2();
-        // let pan_friction = 0.999999;
+        let sd_abs = self.anim.scale_delta.abs();
+
+        let sd_mult = if sd_abs >= 2.0 {
+            1.0
+        } else {
+            let sd_log2 = sd_abs.log2();
+            // if sd_log10 <= 0.0 {
+            //     1.0
+            // } else {
+            -sd_log2
+            // }
+        };
+
+        let sd_mult = sd_mult.max(1.0);
+
+        // let zoom_friction = 1.0 - ((0.9 * t) / (2.0_f32.powf(sd_mult)));
+        // let zoom_friction = 1.0 - (0.9.log10() * t).log(2.78);
+        // let zoom_friction = 1.0 - ((10.0_f32.powf(t)) / 10.0);
+
+        let zoom_friction = 1.0 - (10.0_f32.powf(t - 1.0));
+
         let pan_friction = 1.0 - (0.5 * t);
-        // let pan_friction = 1.0 - (0.75 * t);
 
         let dx = self.anim.view_delta.x;
         let dy = self.anim.view_delta.y;
@@ -144,8 +165,23 @@ impl UIState {
         self.view.center.x += t * dx * self.view.scale;
         self.view.center.y += t * dy * self.view.scale;
 
+        // let sd_log10 = self.anim.scale_delta.log10();
+        // let sd_log10 = sd_abs.log10();
+
+        // "scale {:<16} - delta {:<16} - friction {:<16} - sd_log10 {}",
+        println!(
+            "scale {:<16} - delta {:<16} - friction {:<16} - sd_mult {}",
+            self.view.scale, self.anim.scale_delta, zoom_friction, sd_mult
+        );
+
         self.anim.view_delta *= pan_friction;
         self.anim.scale_delta *= zoom_friction;
+
+        if self.anim.scale_delta.abs() < 0.00001 {
+            self.anim.scale_delta = 0.0;
+        }
+
+        // println!("scale delta {}", self.anim.scale_delta);
 
         // if self.anim.view_delta.x.abs() < 0.01 {
         //     self.anim.view_delta.x = 0.0;
@@ -173,7 +209,8 @@ impl UIState {
             UICmd::Zoom { delta } => {
                 let delta_mult = self.view.scale.log2();
                 let delta_mult = delta_mult.max(1.0);
-                self.view.scale += delta * delta_mult;
+                self.anim.scale_delta += delta * delta_mult;
+                // self.view.scale += delta * delta_mult;
             }
             UICmd::SetCenter { center } => {
                 self.anim.view_delta = Point::default();
