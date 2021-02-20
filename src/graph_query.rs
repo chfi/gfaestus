70410197a4ceeb08d6ef1be56cb49d475df90c93
Christian Fischer <christian@chfi.se>
2021-02-20 17:26:15 -0600

    simple GraphQuery system implementation

---
+++ src/graph_query.rs
@@ -0,0 +1,154 @@
+#[allow(unused_imports)]
+use handlegraph::{
+    handle::{Direction, Handle, NodeId},
+    handlegraph::*,
+    mutablehandlegraph::*,
+    packed::*,
+    pathhandlegraph::*,
+};
+
+use handlegraph::packedgraph::PackedGraph;
+
+use crossbeam::channel;
+
+use std::sync::Arc;
+
+use anyhow::Result;
+
+pub struct GraphQuery {
+    _graph: Arc<PackedGraph>,
+    query_thread: QueryThread,
+}
+
+impl GraphQuery {
+    pub fn new(graph: PackedGraph) -> Self {
+        let graph = Arc::new(graph);
+        let query_thread = QueryThread::new(graph.clone());
+        Self {
+            _graph: graph,
+            query_thread,
+        }
+    }
+
+    pub fn query_request_blocking(
+        &self,
+        request: GraphQueryRequest,
+    ) -> GraphQueryResp {
+        self.query_thread.request_blocking(request)
+    }
+}
+
+struct QueryThread {
+    resp_rx: channel::Receiver<GraphQueryResp>,
+    req_tx: channel::Sender<GraphQueryRequest>,
+    _thread_handle: std::thread::JoinHandle<()>,
+}
+
+impl QueryThread {
+    fn request_blocking(&self, request: GraphQueryRequest) -> GraphQueryResp {
+        self.req_tx.send(request).unwrap();
+        self.resp_rx.recv().unwrap()
+    }
+
+    fn new(graph: Arc<PackedGraph>) -> Self {
+        let (resp_tx, resp_rx) = channel::bounded::<GraphQueryResp>(0);
+        let (req_tx, req_rx) = channel::bounded::<GraphQueryRequest>(0);
+
+        let _thread_handle = std::thread::spawn(move || {
+            use GraphQueryRequest as Req;
+            use GraphQueryResp as Resp;
+
+            use Direction as Dir;
+
+            while let Ok(request) = req_rx.recv() {
+                let resp: Resp = match request {
+                    Req::GraphStats => Resp::GraphStats {
+                        node_count: graph.node_count(),
+                        edge_count: graph.edge_count(),
+                        path_count: graph.path_count(),
+                        total_len: graph.total_length(),
+                    },
+                    Req::NodeStats(node_id) => {
+                        let handle = Handle::pack(node_id, false);
+
+                        let deg_l = graph.degree(handle, Dir::Left);
+                        let deg_r = graph.degree(handle, Dir::Right);
+
+                        let coverage: usize = graph
+                            .steps_on_handle(handle)
+                            .map(|occurs| occurs.count())
+                            .unwrap_or(0);
+
+                        Resp::NodeStats {
+                            node_id,
+                            len: graph.node_len(handle),
+                            degree: (deg_l, deg_r),
+                            coverage,
+                        }
+                    }
+                    Req::PathStats(path_id) => {
+                        let step_count = graph.path_len(path_id).unwrap_or(0);
+                        Resp::PathStats {
+                            path_id,
+                            step_count,
+                        }
+                    }
+                    Req::NodeSeq(node_id) => {
+                        let seq =
+                            graph.sequence_vec(Handle::pack(node_id, false));
+                        let len = seq.len();
+
+                        Resp::NodeSeq { node_id, seq, len }
+                    }
+                };
+
+                resp_tx.send(resp).unwrap();
+            }
+        });
+
+        Self {
+            resp_rx,
+            req_tx,
+            _thread_handle,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum GraphQueryRequest {
+    GraphStats,
+    NodeStats(NodeId),
+    PathStats(PathId),
+    NodeSeq(NodeId),
+    // Neighbors(NodeId),
+}
+
+#[derive(Debug, Clone)]
+pub enum GraphQueryResp {
+    GraphStats {
+        node_count: usize,
+        edge_count: usize,
+        path_count: usize,
+        total_len: usize,
+    },
+    NodeStats {
+        node_id: NodeId,
+        len: usize,
+        degree: (usize, usize),
+        coverage: usize,
+    },
+    PathStats {
+        path_id: PathId,
+        step_count: usize,
+    },
+    NodeSeq {
+        node_id: NodeId,
+        seq: Vec<u8>,
+        len: usize,
+    },
+    // Neighbors {
+    //     node_id: NodeId,
+    //     left: Vec<NodeId>,
+    //     right: Vec<NodeId>,
+    // },
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 pub mod app;
 pub mod geometry;
 pub mod gfa;
+pub mod graph_query;
 pub mod input;
 pub mod render;
 pub mod universe;
