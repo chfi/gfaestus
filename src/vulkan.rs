0b8a27f8ebd28a63f935504183b51c184f5ba862
Christian Fischer <christian@chfi.se>
2021-03-10 19:09:05 -0600

    swapchain stuff

--- src/vulkan.rs
+++ src/vulkan.rs
@@ -28,12 +28,6 @@ use std::{
 
 use anyhow::Result;
 
-struct SwapchainConfig {
-    extent: vk::Extent2D,
-    present_mode: vk::PresentModeKHR,
-    format: vk::SurfaceFormatKHR,
-}
-
 pub struct GfaestusVk {
     vk_context: VkContext,
 
@@ -47,7 +41,7 @@ pub struct GfaestusVk {
 
     swapchain: Swapchain,
     swapchain_khr: vk::SwapchainKHR,
-    swapchain_cfg: SwapchainConfig,
+    swapchain_props: SwapchainProperties,
 
     swapchain_images: Vec<vk::Image>,
     swapchain_image_views: Vec<vk::ImageView>,
@@ -170,12 +164,151 @@ fn device_is_suitable(
     let (graphics_ix, present_ix) =
         find_queue_families(instance, surface, surface_khr, device)?;
 
-    let supports_extensions = device_supports_extensions(instance, device)?;
+    if graphics_ix.is_none() || present_ix.is_none() {
+        return Ok(false);
+    }
 
-    // let swapchain_adequate = {
-    //     let details = Swap
-    // };
+    if !device_supports_extensions(instance, device)? {
+        return Ok(false);
+    }
 
-    unimplemented!();
+    let swapchain_adequate = {
+        let details =
+            SwapchainSupportDetails::new(device, surface, surface_khr)?;
+        !details.formats.is_empty() && !details.present_modes.is_empty()
+    };
+
+    if !swapchain_adequate {
+        return Ok(false);
+    }
+
+    let features = unsafe { instance.get_physical_device_features(device) };
+
+    // TODO this should be tailored
+    Ok(features.sampler_anisotropy == vk::TRUE)
 }
 
+
+}
+
+struct SwapchainProperties {
+    extent: vk::Extent2D,
+    present_mode: vk::PresentModeKHR,
+    format: vk::SurfaceFormatKHR,
+}
+
+struct SwapchainSupportDetails {
+    capabilities: vk::SurfaceCapabilitiesKHR,
+    formats: Vec<vk::SurfaceFormatKHR>,
+    present_modes: Vec<vk::PresentModeKHR>,
+}
+
+impl SwapchainSupportDetails {
+    fn new(
+        device: vk::PhysicalDevice,
+        surface: &Surface,
+        surface_khr: vk::SurfaceKHR,
+    ) -> Result<Self> {
+        unsafe {
+            let capabilities = surface
+                .get_physical_device_surface_capabilities(
+                    device,
+                    surface_khr,
+                )?;
+
+            let formats = surface
+                .get_physical_device_surface_formats(device, surface_khr)?;
+
+            let present_modes = surface
+                .get_physical_device_surface_present_modes(
+                    device,
+                    surface_khr,
+                )?;
+
+            Ok(Self {
+                capabilities,
+                formats,
+                present_modes,
+            })
+        }
+    }
+
+    fn get_ideal_swapchain_properties(
+        &self,
+        preferred_dimensions: [u32; 2],
+    ) -> SwapchainProperties {
+        let format = Self::choose_swapchain_surface_format(&self.formats);
+        let present_mode =
+            Self::choose_swapchain_surface_present_mode(&self.present_modes);
+        let extent = Self::choose_swapchain_extent(
+            self.capabilities,
+            preferred_dimensions,
+        );
+        SwapchainProperties {
+            format,
+            present_mode,
+            extent,
+        }
+    }
+
+    /// Choose the swapchain surface format.
+    ///
+    /// Will choose B8G8R8A8_UNORM/SRGB_NONLINEAR if possible or
+    /// the first available otherwise.
+    fn choose_swapchain_surface_format(
+        available_formats: &[vk::SurfaceFormatKHR],
+    ) -> vk::SurfaceFormatKHR {
+        if available_formats.len() == 1
+            && available_formats[0].format == vk::Format::UNDEFINED
+        {
+            return vk::SurfaceFormatKHR {
+                format: vk::Format::B8G8R8A8_UNORM,
+                color_space: vk::ColorSpaceKHR::SRGB_NONLINEAR,
+            };
+        }
+
+        *available_formats
+            .iter()
+            .find(|format| {
+                format.format == vk::Format::B8G8R8A8_UNORM
+                    && format.color_space == vk::ColorSpaceKHR::SRGB_NONLINEAR
+            })
+            .unwrap_or(&available_formats[0])
+    }
+
+    /// Choose the swapchain present mode.
+    ///
+    /// Will favor MAILBOX if present otherwise FIFO.
+    /// If none is present it will fallback to IMMEDIATE.
+    fn choose_swapchain_surface_present_mode(
+        available_present_modes: &[vk::PresentModeKHR],
+    ) -> vk::PresentModeKHR {
+        if available_present_modes.contains(&vk::PresentModeKHR::MAILBOX) {
+            vk::PresentModeKHR::MAILBOX
+        } else if available_present_modes.contains(&vk::PresentModeKHR::FIFO) {
+            vk::PresentModeKHR::FIFO
+        } else {
+            vk::PresentModeKHR::IMMEDIATE
+        }
+    }
+
+    /// Choose the swapchain extent.
+    ///
+    /// If a current extent is defined it will be returned.
+    /// Otherwise the surface extent clamped between the min
+    /// and max image extent will be returned.
+    fn choose_swapchain_extent(
+        capabilities: vk::SurfaceCapabilitiesKHR,
+        preferred_dimensions: [u32; 2],
+    ) -> vk::Extent2D {
+        if capabilities.current_extent.width != std::u32::MAX {
+            return capabilities.current_extent;
+        }
+
+        let min = capabilities.min_image_extent;
+        let max = capabilities.max_image_extent;
+        let width = preferred_dimensions[0].min(max.width).max(min.width);
+        let height = preferred_dimensions[1].min(max.height).max(min.height);
+        vk::Extent2D { width, height }
+    }
+}
