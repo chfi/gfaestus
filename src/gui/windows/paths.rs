f3f56d919ea51fe033f1b4eb7baf5f507d9d9d36
Christian Fischer <christian@chfi.se>
2021-05-07 16:03:08 -0500

    create & update path list state

--- src/gui/windows/paths.rs
+++ src/gui/windows/paths.rs
@@ -52,7 +52,7 @@ pub struct PathListSlot {
 #[derive(Debug, Clone)]
 pub struct PathDetails {
     path_id: Arc<AtomicCell<Option<PathId>>>,
-    path_name: String,
+    path_name: Vec<u8>,
     fetched_path: Option<PathId>,
 
     head: StepPtr,
@@ -66,7 +66,7 @@ impl std::default::Default for PathDetails {
     fn default() -> Self {
         Self {
             path_id: Arc::new(AtomicCell::new(None)),
-            path_name: String::new(),
+            path_name: Vec::new(),
             fetched_path: None,
 
             head: StepPtr::null(),
@@ -75,5 +75,115 @@ impl std::default::Default for PathDetails {
             step_count: 0,
             base_count: 0,
         }
+    }
+}
+
+impl PathList {
+    const ID: &'static str = "path_list_window";
+
+    pub fn ui(
+        &mut self,
+        ctx: &egui::CtxRef,
+        app_msg_tx: &Sender<AppMsg>,
+        open_path_details: &mut bool,
+        graph_query: &GraphQuery,
+    ) -> Option<egui::Response> {
+        unimplemented!();
+    }
+
+    pub fn new(
+        graph_query: &GraphQuery,
+        page_size: usize,
+        path_details_id: Arc<AtomicCell<Option<PathId>>>,
+    ) -> Self {
+        let graph = graph_query.graph();
+        let path_count = graph.path_count();
+
+        let mut all_paths = graph.path_ids().collect::<Vec<_>>();
+        all_paths.sort();
+
+        let page = 0;
+        let page_count = path_count / page_size;
+
+        let mut slots: Vec<PathListSlot> = Vec::with_capacity(page_size);
+
+        for &path in all_paths[0..page_size].iter() {
+            slots.push(PathListSlot::default());
+        }
+
+        let update_slots = false;
+
+        Self {
+            all_paths,
+
+            page,
+            page_size,
+            page_count,
+
+            slots,
+
+            update_slots,
+
+            path_details_id,
+        }
+    }
+
+    pub fn apply_msg(&mut self, msg: PathListMsg) {
+        match msg {
+            PathListMsg::NextPage => {
+                if self.page < self.page_count {
+                    self.page += 1;
+                    self.update_slots = true;
+                }
+            }
+            PathListMsg::PrevPage => {
+                if self.page > 0 {
+                    self.page -= 1;
+                    self.update_slots = true;
+                }
+            }
+            PathListMsg::SetPage(page) => {
+                let page = page.clamp(0, self.page_count);
+                if self.page != page {
+                    self.page = page;
+                    self.update_slots = true;
+                }
+            }
+        }
+    }
+
+    fn update_slots(&mut self, graph_query: &GraphQuery, force_update: bool) {
+        if !self.update_slots && !force_update {
+            return;
+        }
+
+        let paths = &self.all_paths;
+
+        let page_start =
+            (self.page * self.page_size).min(paths.len() - (paths.len() % self.page_size));
+        let page_end = (page_start + self.page_size).min(paths.len());
+
+        for slot in self.slots.iter_mut() {
+            slot.path_details.path_id.store(None);
+        }
+
+        for (slot, path) in self.slots.iter_mut().zip(&paths[page_start..page_end]) {
+            let slot = &mut slot.path_details;
+            slot.path_id.store(Some(*path));
+
+            slot.path_name.clear();
+            slot.path_name
+                .extend(graph_query.graph().get_path_name_vec(*path).unwrap());
+
+            slot.head = graph_query.graph().path_first_step(*path).unwrap();
+            slot.tail = graph_query.graph().path_last_step(*path).unwrap();
+
+            slot.step_count = graph_query.graph().path_len(*path).unwrap();
+            slot.base_count = graph_query.graph().path_bases_len(*path).unwrap();
+
+            slot.fetched_path = Some(*path);
+        }
+
+        self.update_slots = false;
     }
 }
