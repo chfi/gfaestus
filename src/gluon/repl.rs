f2ab003c81af53aace0582c8e4c7ca7035ddef1e
Christian Fischer <christian@chfi.se>
2021-05-17 20:24:33 -0500

    most of core repl logic

--- src/gluon/repl.rs
+++ src/gluon/repl.rs
@@ -1,8 +1,8 @@
-use std::{path::Path, sync::Arc};
+use std::{path::Path, str::FromStr, sync::Arc};
 
 use bstr::ByteVec;
 use crossbeam::channel::Sender;
-use futures::Future;
+use futures::{Future, FutureExt};
 use gluon_codegen::*;
 
 use gluon::{
@@ -23,10 +23,14 @@ use gluon::{
     query::CompilerDatabase,
     vm::{
         api::{FunctionRef, Hole, OpaqueValue, WithVM, IO},
+        vm::RootedValue,
         ExternModule, {self, Error as VMError, Result as VMResult},
     },
-    Error as GluonError, Result as GluonResult, RootedThread, ThreadExt,
+    Error as GluonError, Result as GluonResult, RootedThread, Thread,
+    ThreadExt,
 };
+
+use gluon::parser::{parse_partial_repl_line, ReplLine};
 
 use gluon_completion as completion;
 
@@ -39,6 +43,8 @@ use crate::{
     geometry::Point,
     view::View,
 };
+
+// taken and modified from gluon_repl
 
 fn type_of_expr(
     args: WithVM<&str>,
@@ -58,7 +64,7 @@ fn type_of_expr(
     }
 }
 
-fn find_kind(args: WithVM<&str>) -> IO<Result<String>> {
+fn find_kind(args: WithVM<&str>) -> IO<std::result::Result<String, String>> {
     let vm = args.vm;
     let args = args.value.trim();
     IO::Value(match vm.find_type_info(args) {
@@ -73,8 +79,7 @@ fn find_kind(args: WithVM<&str>) -> IO<Result<String>> {
     })
 }
 
-// fn find_info(args: WithVM<&str>) -> IO<Result<String, String>> {
-fn find_info(args: WithVM<&str>) -> IO<Result<String>> {
+fn find_info(args: WithVM<&str>) -> IO<std::result::Result<String, String>> {
     use std::fmt::Write;
     let vm = args.vm;
     let args = args.value.trim();
@@ -169,4 +174,248 @@ fn complete(
             s.to_string()
         })
         .collect())
+}
+
+fn eval_line(
+    WithVM { vm, value: line }: WithVM<&str>,
+) -> impl Future<Output = IO<()>> {
+    let vm = vm.new_thread().unwrap(); // TODO Reuse the current thread
+    let line = line.to_string();
+    async move {
+        eval_line_(vm.root_thread(), &line)
+            .map(move |result| match result {
+                Ok(x) => IO::Value(x),
+                Err(err) => {
+                    // if let Err(err) = err.emit(&mut stderr) {
+                    eprintln!("{}", err);
+                    // }
+                    IO::Value(())
+                }
+            })
+            .await
+    }
+}
+
+async fn eval_line_(vm: RootedThread, line: &str) -> gluon::Result<()> {
+    let mut is_let_binding = false;
+    let mut eval_expr;
+    let value = {
+        let mut db = vm.get_database();
+        let mut module_compiler = vm.module_compiler(&mut db);
+        eval_expr = {
+            let eval_expr = {
+                mk_ast_arena!(arena);
+                let repl_line = {
+                    let result = {
+                        let filemap =
+                            vm.get_database().add_filemap("line", line);
+                        let mut module = SymbolModule::new(
+                            "line".into(),
+                            module_compiler.mut_symbols(),
+                        );
+                        parse_partial_repl_line(
+                            (*arena).borrow(),
+                            &mut module,
+                            &*filemap,
+                        )
+                    };
+                    match result {
+                        Ok(x) => x,
+                        Err((_, err)) => {
+                            let code_map = db.code_map();
+                            return Err(InFile::new(code_map, err).into());
+                        }
+                    }
+                };
+                match repl_line {
+                    None => return Ok(()),
+                    Some(ReplLine::Expr(expr)) => {
+                        RootExpr::new(arena.clone(), arena.alloc(expr))
+                    }
+                    Some(ReplLine::Let(let_binding)) => {
+                        is_let_binding = true;
+                        // We can't compile function bindings by only looking at `let_binding.expr`
+                        // so rewrite `let f x y = <expr>` into `let f x y = <expr> in f`
+                        // and `let { x } = <expr>` into `let repl_temp @ { x } = <expr> in repl_temp`
+                        let id = match let_binding.name.value {
+                            Pattern::Ident(ref id)
+                                if !let_binding.args.is_empty() =>
+                            {
+                                id.clone()
+                            }
+                            _ => {
+                                let id = Symbol::from("repl_temp");
+                                let_binding.name = pos::spanned(
+                                    let_binding.name.span,
+                                    Pattern::As(
+                                        pos::spanned(
+                                            let_binding.name.span,
+                                            id.clone(),
+                                        ),
+                                        arena.alloc(
+                                            let_binding
+                                                .name
+                                                .ast_clone(arena.borrow()),
+                                        ),
+                                    ),
+                                );
+                                TypedIdent {
+                                    name: id,
+                                    typ: let_binding.resolved_type.clone(),
+                                }
+                            }
+                        };
+                        let id = pos::spanned2(
+                            0.into(),
+                            0.into(),
+                            Expr::Ident(id.clone()),
+                        );
+                        let expr = Expr::LetBindings(
+                            ast::ValueBindings::Plain(let_binding),
+                            arena.alloc(id),
+                        );
+                        let eval_expr = RootExpr::new(
+                            arena.clone(),
+                            arena.alloc(pos::spanned2(
+                                0.into(),
+                                0.into(),
+                                expr,
+                            )),
+                        );
+                        eval_expr
+                    }
+                }
+            };
+            eval_expr.try_into_send().unwrap()
+        };
+
+        (&mut eval_expr)
+            .run_expr(&mut module_compiler, vm.clone(), "line", line, None)
+            .await?
+    };
+    let ExecuteValue { value, typ, .. } = value;
+
+    if is_let_binding {
+        let mut expr = eval_expr.expr();
+        let mut last_bind = None;
+        loop {
+            match &expr.value {
+                Expr::LetBindings(binds, body) => {
+                    last_bind = Some(&binds[0]);
+                    expr = body;
+                }
+                _ => break,
+            }
+        }
+        set_globals(
+            &vm,
+            &mut vm.get_database_mut(),
+            &last_bind.unwrap().name,
+            &typ,
+            &value.as_ref(),
+        )?;
+    }
+    let vm = value.vm();
+    let env = vm.get_env();
+    let debug_level = vm.global_env().get_debug_level();
+    // println!(
+    //     "{}",
+    //     ValuePrinter::new(&env, &typ, value.get_variant(), &debug_level)
+    //         .width(80)
+    //         .max_level(5)
+    // );
+    Ok(())
+}
+
+fn set_globals(
+    vm: &Thread,
+    db: &mut CompilerDatabase,
+    pattern: &SpannedPattern<Symbol>,
+    typ: &ArcType,
+    value: &RootedValue<&Thread>,
+) -> GluonResult<()> {
+    match pattern.value {
+        Pattern::Ident(ref id) => {
+            db.set_global(
+                id.name.declared_name(),
+                typ.clone(),
+                Default::default(),
+                value.get_value(),
+            );
+            Ok(())
+        }
+        Pattern::Tuple { ref elems, .. } => {
+            let iter = elems
+                .iter()
+                .zip(gluon::vm::dynamic::field_iter(&value, typ, vm));
+            for (elem_pattern, (elem_value, elem_type)) in iter {
+                set_globals(vm, db, elem_pattern, &elem_type, &elem_value)?;
+            }
+            Ok(())
+        }
+        Pattern::Record { ref fields, .. } => {
+            let resolved_type = {
+                let mut type_cache = vm.global_env().type_cache();
+                let env = db.as_env();
+                resolve::remove_aliases_cow(&env, &mut type_cache, typ)
+            };
+
+            for (name, pattern_value) in ast::pattern_values(fields) {
+                let field_name: &Symbol = &name.value;
+                // if the record didn't have a field with this name,
+                // there should have already been a type error. So we can just panic here
+                let field_value: RootedValue<&Thread> = value
+                    .get_field(field_name.declared_name())
+                    .unwrap_or_else(|| {
+                        panic!(
+                            "record doesn't have field `{}`",
+                            field_name.declared_name()
+                        )
+                    });
+                let field_type = resolved_type
+                    .row_iter()
+                    .find(|f| f.name.name_eq(field_name))
+                    .unwrap_or_else(|| {
+                        panic!(
+                            "record type `{}` doesn't have field `{}`",
+                            resolved_type,
+                            field_name.declared_name()
+                        )
+                    })
+                    .typ
+                    .clone();
+                match pattern_value {
+                    Some(ref sub_pattern) => set_globals(
+                        vm,
+                        db,
+                        sub_pattern,
+                        &field_type,
+                        &field_value,
+                    )?,
+                    None => db.set_global(
+                        name.value.declared_name(),
+                        field_type.to_owned(),
+                        Default::default(),
+                        field_value.get_value(),
+                    ),
+                }
+            }
+            Ok(())
+        }
+        Pattern::As(ref id, ref pattern) => {
+            db.set_global(
+                id.value.declared_name(),
+                typ.clone(),
+                Default::default(),
+                value.get_value(),
+            );
+            set_globals(vm, db, pattern, typ, value)
+        }
+        Pattern::Constructor(..) | Pattern::Literal(_) | Pattern::Error => {
+            Err(VMError::Message(
+                "The repl cannot bind variables from this pattern".into(),
+            )
+            .into())
+        }
+    }
 }
