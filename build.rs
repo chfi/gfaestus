cf4f34a5dddeb467e25fd6ef47335e04c808b147
Christian Fischer <christian@chfi.se>
2021-05-20 18:25:49 -0500

    compile shaders in subdirectories

--- build.rs
+++ build.rs
@@ -7,15 +7,19 @@ use std::{
 };
 
 fn main() {
+    if Command::new("glslangValidator").output().is_err() {
+        eprintln!("Error compiling shaders: 'glslangValidator' not found, do you have the Vulkan SDK installed?");
+        eprintln!("Get it at https://vulkan.lunarg.com/");
+        std::process::exit(1);
+    }
+
     let shader_files = find_shader_files();
 
     for path in shader_files.iter() {
         println!("cargo:rerun-if-changed={}", path.to_str().unwrap());
     }
 
-    if !skip_compiling_shaders() {
-        compile_shaders()
-    }
+    compile_shaders(&shader_files)
 }
 
 fn find_shader_files() -> Vec<PathBuf> {
@@ -44,46 +48,24 @@ fn find_shader_files() -> Vec<PathBuf> {
     result
 }
 
-fn skip_compiling_shaders() -> bool {
-    if let Ok(v) = std::env::var("SKIP_SHADER_COMPILATION") {
-        v.parse::<bool>().unwrap_or(false)
-    } else {
-        false
+fn compile_shaders(files: &[PathBuf]) {
+    for path in files {
+        let name = path.file_name().unwrap().to_str().unwrap();
+        let output_name = format!("{}.spv", &name);
+
+        let mut output_path = path.clone();
+        output_path.pop();
+        output_path.push(output_name);
+
+        let result = Command::new("glslangValidator")
+            .arg("-V")
+            .arg(&path)
+            .arg("-o")
+            .arg(output_path)
+            .output();
+
+        handle_program_result(result);
     }
-}
-
-fn compile_shaders() {
-    println!("Compiling shaders...");
-
-    let shader_dir_path = Path::new(env!("CARGO_MANIFEST_DIR")).join("shaders");
-
-    std::fs::read_dir(shader_dir_path.clone())
-        .unwrap()
-        .map(Result::unwrap)
-        .filter(|dir| {
-            dir.file_type().unwrap().is_file() && dir.path().extension() != Some(OsStr::new("spv"))
-        })
-        .for_each(|file| {
-            let path = file.path();
-            let name = path.file_name().unwrap().to_str().unwrap();
-            let output_name = format!("{}.spv", &name);
-
-            if Command::new("glslangValidator").output().is_err() {
-                eprintln!("Error compiling shaders: 'glslangValidator' not found, do you have the Vulkan SDK installed?");
-                eprintln!("Get it at https://vulkan.lunarg.com/");
-                std::process::exit(1);
-            }
-
-            let result = Command::new("glslangValidator")
-                .current_dir(&shader_dir_path)
-                .arg("-V")
-                .arg(&path)
-                .arg("-o")
-                .arg(output_name)
-                .output();
-
-            handle_program_result(result);
-        })
 }
 
 fn handle_program_result(result: Result<Output>) {
