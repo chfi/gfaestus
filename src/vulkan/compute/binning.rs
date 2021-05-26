840ddcb92bd50f64b6bf6e84b4ccc5d2438417e1
Christian Fischer <christian@chfi.se>
2021-05-26 14:29:16 -0500

    define binning compute pipelines & desc sets

--- src/vulkan/compute/binning.rs
+++ src/vulkan/compute/binning.rs
@@ -11,6 +11,129 @@ use crate::vulkan::{draw_system::nodes::NodeVertices, GfaestusVk};
 
 use super::{ComputeManager, ComputePipeline};
 
+pub struct Binner {
+    step_1: ComputePipeline,
+    step_2: ComputePipeline,
+    step_3: ComputePipeline,
+
+    buffers: BinBuffers,
+}
+
+impl Binner {
+    pub fn new(app: &GfaestusVk, node_count: usize) -> Result<Self> {
+        let bin_count = 32 * 32; // roughly close choice for now
+
+        let buffers = BinBuffers::new(app, node_count, bin_count)?;
+
+        let device = app.vk_context().device();
+
+        let step_1 = {
+            let (desc_set_layout, pipeline_layout) = Self::layouts(device)?;
+
+            ComputePipeline::new(
+                device,
+                desc_set_layout,
+                pipeline_layout,
+                crate::include_shader!("compute/bin1.comp.spv"),
+            )
+        };
+
+        let step_2 = {
+            let (desc_set_layout, pipeline_layout) = Self::layouts(device)?;
+
+            ComputePipeline::new(
+                device,
+                desc_set_layout,
+                pipeline_layout,
+                crate::include_shader!("compute/bin2.comp.spv"),
+            )
+        };
+
+        let step_3 = {
+            let (desc_set_layout, pipeline_layout) = Self::layouts(device)?;
+
+            ComputePipeline::new(
+                device,
+                desc_set_layout,
+                pipeline_layout,
+                crate::include_shader!("compute/bin3.comp.spv"),
+            )
+        };
+
+        Ok(Self {
+            buffers,
+            step_1,
+            step_2,
+            step_3,
+        })
+    }
+
+    fn layouts(
+        device: &Device,
+    ) -> Result<(vk::DescriptorSetLayout, vk::PipelineLayout)> {
+        let desc_set_layout = Self::create_descriptor_set_layout(device)?;
+
+        let pipeline_layout = {
+            use vk::ShaderStageFlags as Flags;
+
+            let pc_range = vk::PushConstantRange::builder()
+                .stage_flags(Flags::COMPUTE)
+                .offset(0)
+                .size(24)
+                .build();
+
+            let pc_ranges = [pc_range];
+
+            let layouts = [desc_set_layout];
+
+            let layout_info = vk::PipelineLayoutCreateInfo::builder()
+                .set_layouts(&layouts)
+                .push_constant_ranges(&pc_ranges)
+                .build();
+
+            unsafe { device.create_pipeline_layout(&layout_info, None) }
+        }?;
+
+        Ok((desc_set_layout, pipeline_layout))
+    }
+
+    fn layout_binding() -> [vk::DescriptorSetLayoutBinding; 5] {
+        use vk::ShaderStageFlags as Stages;
+
+        let mk_builder = |binding: u32| {
+            vk::DescriptorSetLayoutBinding::builder()
+                .binding(binding)
+                .descriptor_type(vk::DescriptorType::STORAGE_BUFFER)
+                .descriptor_count(1)
+                .stage_flags(Stages::COMPUTE)
+                .build()
+        };
+
+        let nodes = mk_builder(0);
+        let node_bins = mk_builder(1);
+        let node_bin_offsets = mk_builder(2);
+        let bin_offsets = mk_builder(3);
+        let bins = mk_builder(4);
+
+        [nodes, node_bins, node_bin_offsets, bin_offsets, bins]
+    }
+
+    fn create_descriptor_set_layout(
+        device: &Device,
+    ) -> Result<vk::DescriptorSetLayout> {
+        let bindings = Self::layout_binding();
+
+        let layout_info = vk::DescriptorSetLayoutCreateInfo::builder()
+            .bindings(&bindings)
+            .build();
+
+        let layout =
+            unsafe { device.create_descriptor_set_layout(&layout_info, None) }?;
+
+        Ok(layout)
+    }
+}
+
 pub struct ComputeBuffer {
     pub(super) buffer: vk::Buffer,
     pub(super) memory: vk::DeviceMemory,
