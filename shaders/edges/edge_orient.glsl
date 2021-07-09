1131275bc8d66028c3246f9b76e7342916d1aa8f
Christian Fischer <christian@chfi.se>
2021-07-09 11:43:41 -0500

    fix edge_orient.glsl logic

--- shaders/edges/edge_orient.glsl
+++ shaders/edges/edge_orient.glsl
@@ -1,26 +1,38 @@
 uvec2 oriented_edge_ixs(uvec2 handles) {
 
-  uint left_id = handles.x >> 1;
-  uint right_id = handles.y >> 1;
+  uint left_id   = (handles.x >> 1) - 1;
+  uint right_id  = (handles.y >> 1) - 1;
 
-  bool left_fwd = (handles.x & 1) != 1;
-  bool right_fwd = (handles.y & 1) != 1;
+  bool left_rev  = (handles.x & 1) == 1;
+  bool right_rev = (handles.x & 1) == 1;
+
+
+  uint left_l = left_id * 2;
+  uint left_r = left_l + 1;
+
+  uint right_l = right_id * 2;
+  uint right_r = right_l + 1;
+
 
   uint left_ix;
   uint right_ix;
 
-  if (left_fwd && right_fwd) {
-    left_ix = handles.x + 1;
-    right_ix = handles.y;
-  } else if (!left_fwd && right_fwd) {
-    left_ix = handles.x;
-    right_ix = handles.y - 1;
-  } else if (left_fwd && !right_fwd) {
-    left_ix = handles.x;
-    right_ix = handles.y;
-  } else if (!left_fwd && !right_fwd) {
-    left_ix = handles.x;
-    right_ix = handles.y + 1;
+  if (!left_rev && !right_rev) {
+    left_ix = left_r;
+    right_ix = right_l;
+
+  } else if (left_rev && !right_rev) {
+    left_ix = left_l;
+    right_ix = right_l;
+
+  } else if (!left_rev && right_rev) {
+    left_ix = left_r;
+    right_ix = right_r;
+
+  } else if (left_rev && right_rev) {
+    left_ix = left_l;
+    right_ix = right_r;
+
   }
 
   return uvec2(left_ix, right_ix);
