8ce67edbf2697ad38206e66e5883858b8c9a02bc
Christian Fischer <christian@chfi.se>
2021-08-18 15:52:04 -0500

    designing an async execution graph using channels

--- src/reactor.rs
+++ src/reactor.rs
@@ -1,12 +1,53 @@
-use std::{collections::VecDeque, path::PathBuf, sync::Arc};
+use std::{any::TypeId, collections::VecDeque, path::PathBuf, sync::Arc};
 
 use crate::{
     graph_query::GraphQuery, gui::windows::OverlayCreatorMsg,
     overlays::OverlayData, script::ScriptConfig,
 };
 
-use crossbeam::channel::{Receiver, Sender};
+use crossbeam::channel::{Receiver, Sender, TryRecvError};
 use handlegraph::packedgraph::PackedGraph;
+use rustc_hash::FxHashMap;
+
+pub struct ReactorOutput {
+    sender: Box<dyn std::any::Any>, // should always be a sender, but this part won't be exposed in the API,
+    output_type: TypeId,
+}
+
+// pub struct ReactorOutputReceiver<T: Send + Sync + std::any::Any> {
+pub struct ReactorOutputReceiver<T: Send + Sync> {
+    receiver: Receiver<T>,
+    id: ReactorOutputId,
+    results: Option<T>,
+}
+
+impl<T: Send + Sync> ReactorOutputReceiver<T> {
+    pub fn try_recv(&mut self) -> Result<&T, TryRecvError> {
+        let result = self.receiver.try_recv()?;
+        self.results = Some(result);
+        let v = self.results.as_ref().unwrap();
+        Ok(v)
+    }
+
+    pub fn try_results(&mut self) -> Option<&T> {
+        if self.results.is_some() {
+            return self.results.as_ref();
+        }
+
+        let result = self.receiver.try_recv().ok()?;
+        self.results = Some(result);
+        self.results.as_ref()
+    }
+
+    pub fn results(&self) -> Option<&T> {
+        self.results.as_ref()
+    }
+}
+
+pub struct ReactorOutputTyped<T: Send + Sync + 'static> {
+    sender: Box<Sender<T>>,
+    output_type: T,
+}
 
 pub struct Reactor {
     queue: VecDeque<Message>,
@@ -14,6 +55,53 @@ pub struct Reactor {
     futures_pool: futures::executor::ThreadPool,
 
     message_in: crossbeam::channel::Receiver<Message>,
+
+    graph: Arc<GraphQuery>,
+
+    outputs: Vec<ReactorOutput>,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub struct ReactorOutputId(pub usize, TypeId);
+// pub struct ReactorOutputId(pub usize, pub TypeId);
+
+impl Reactor {
+    pub fn add_output_chan<T: Send + Sync + 'static>(
+        &mut self,
+    ) -> ReactorOutputReceiver<T> {
+        use std::any::Any;
+
+        let (sender, receiver) = crossbeam::channel::unbounded::<T>();
+
+        let sender = Box::new(sender) as Box<dyn std::any::Any>;
+        let output_type = sender.type_id();
+
+        let next_id = ReactorOutputId(self.outputs.len(), output_type);
+
+        let output = ReactorOutput {
+            sender,
+            output_type,
+        };
+
+        self.outputs.push(output);
+
+        ReactorOutputReceiver {
+            receiver,
+            id: next_id,
+            results: None,
+        }
+    }
+
+    pub fn verify_output_id<T: Send + Sync + 'static>(
+        &self,
+        id: ReactorOutputId,
+    ) -> bool {
+        let ReactorOutputId(ix, type_id) = id;
+
+        let output = &self.outputs[ix];
+
+        output.output_type == type_id
+    }
 }
 
 #[derive(Clone)]
@@ -22,7 +110,6 @@ pub struct Message {
 }
 
 pub struct OverlayMsg {
-    data: OverlayData,
     name: String,
     script_path: PathBuf,
     config: ScriptConfig,
@@ -60,6 +147,21 @@ impl OverlayMsg {
             self.graph.as_ref(),
             &script,
         );
+
+        match overlay_data {
+            Ok(data) => {
+                ok_chan
+                    .send(OverlayCreatorMsg::NewOverlay {
+                        name: self.name.to_owned(),
+                        data,
+                    })
+                    .unwrap();
+                gui_chan.send("Overlay created".to_string()).unwrap();
+            }
+            Err(err) => {
+                gui_chan.send(format!("Script error: {:?}", err)).unwrap();
+            }
+        }
     }
 }
 
