dff6754b07f3c26188293a63b7b32ab3fed9ab64
Christian Fischer <christian@chfi.se>
2021-11-11 15:16:53 -0600

    initialize PathViewRenderer

--- src/vulkan/compute/path_view.rs
+++ src/vulkan/compute/path_view.rs
@@ -1,10 +1,12 @@
 use crate::geometry::{Point, Rect};
+use crate::reactor::Reactor;
 
 use ash::version::DeviceV1_0;
 use ash::{vk, Device};
 
 use anyhow::Result;
 
+use handlegraph::pathhandlegraph::PathId;
 #[allow(unused_imports)]
 use log::{debug, error, info, trace, warn};
 
@@ -16,13 +18,122 @@ use super::{ComputeManager, ComputePipeline};
 
 pub struct PathViewRenderer {
     pipeline: ComputePipeline,
+    descriptor_set_layout: vk::DescriptorSetLayout,
+
+    width: usize,
+    height: usize,
+
+    path_buffer: vk::Buffer,
+    path_allocation: vk_mem::Allocation,
+    path_allocation_info: vk_mem::AllocationInfo,
+    // path_allocation_info: Option<vk_mem::AllocationInfo>,
+    output_buffer: vk::Buffer,
+    output_allocation: vk_mem::Allocation,
+    output_allocation_info: vk_mem::AllocationInfo,
     // path_buffer:
 }
 
 impl PathViewRenderer {
     pub fn new(app: &GfaestusVk) -> Result<Self> {
-        //
-        unimplemented!();
+        let width = 2048;
+        let height = 64;
+        let size = width * height;
+
+        let device = app.vk_context().device();
+
+        let (path_buffer, path_allocation, path_allocation_info) = {
+            let usage = vk::BufferUsageFlags::STORAGE_BUFFER
+                | vk::BufferUsageFlags::TRANSFER_DST;
+            // | vk::BufferUsageFlags::TRANSFER_SRC;
+            let memory_usage = vk_mem::MemoryUsage::CpuToGpu;
+
+            let data = vec![0u32; size];
+
+            let (buffer, allocation, allocation_info) =
+                app.create_buffer_with_data(usage, memory_usage, true, &data)?;
+
+            app.set_debug_object_name(
+                buffer,
+                "Path View Renderer (Path Buffer)",
+            )?;
+
+            (buffer, allocation, allocation_info)
+        };
+
+        let (output_buffer, output_allocation, output_allocation_info) = {
+            let usage = vk::BufferUsageFlags::STORAGE_BUFFER
+                | vk::BufferUsageFlags::TRANSFER_SRC;
+
+            let memory_usage = vk_mem::MemoryUsage::GpuToCpu;
+
+            let pixels = vec![[0u8; 4]; size];
+
+            let (buffer, allocation, allocation_info) = app
+                .create_buffer_with_data(usage, memory_usage, false, &pixels)?;
+
+            app.set_debug_object_name(
+                buffer,
+                "Path View Renderer (Output Buffer)",
+            )?;
+
+            (buffer, allocation, allocation_info)
+        };
+
+        let descriptor_set_layout = Self::create_descriptor_set_layout(device)?;
+
+        let pipeline_layout = {
+            // use vk::ShaderStageFlags as Flags;
+
+            // let pc_range = vk::PushConstantRange::builder()
+            //     .stage_flags(Flags::COMPUTE)
+            //     .offset(0)
+            //     .size(20)
+            //     .build();
+
+            // let pc_ranges = [pc_range];
+            let pc_ranges = [];
+
+            let layouts = [descriptor_set_layout];
+
+            let layout_info = vk::PipelineLayoutCreateInfo::builder()
+                .set_layouts(&layouts)
+                .push_constant_ranges(&pc_ranges)
+                .build();
+
+            unsafe { device.create_pipeline_layout(&layout_info, None) }
+        }?;
+
+        let pipeline = ComputePipeline::new(
+            device,
+            descriptor_set_layout,
+            pipeline_layout,
+            crate::include_shader!("compute/path_view.comp.spv"),
+        )?;
+
+        Ok(Self {
+            pipeline,
+            descriptor_set_layout,
+
+            width,
+            height,
+
+            path_buffer,
+            path_allocation,
+            path_allocation_info,
+
+            output_buffer,
+            output_allocation,
+            output_allocation_info,
+        })
+    }
+
+    pub fn load_paths(
+        &mut self,
+        reactor: &mut Reactor,
+        paths: impl IntoIterator<Item = PathId>,
+    ) -> Result<()> {
+
+        // TODO for now hardcoded to max 64 paths
     }
 
     fn layout_binding() -> [vk::DescriptorSetLayoutBinding; 2] {
@@ -30,21 +141,21 @@ impl PathViewRenderer {
 
         //
 
-        let selection = vk::DescriptorSetLayoutBinding::builder()
+        let path_buffer = vk::DescriptorSetLayoutBinding::builder()
             .binding(0)
             .descriptor_type(vk::DescriptorType::STORAGE_BUFFER)
             .descriptor_count(1)
             .stage_flags(Stages::COMPUTE)
             .build();
 
-        let node_vertices = vk::DescriptorSetLayoutBinding::builder()
+        let output_buffer = vk::DescriptorSetLayoutBinding::builder()
             .binding(1)
             .descriptor_type(vk::DescriptorType::STORAGE_BUFFER)
             .descriptor_count(1)
             .stage_flags(Stages::COMPUTE)
             .build();
 
-        [selection, node_vertices]
+        [path_buffer, output_buffer]
     }
 
     fn create_descriptor_set_layout(
